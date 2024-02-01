7cd7e7defcdacb053c4bda688476810b6f3b3ce3
ShaharGdn <shahargadon@gmail.com>
2024-02-01 17:16:37 +0200

    Implement function to swap balls colors and sizes

--- main.js
+++ main.js
@@ -15,6 +15,27 @@ function onBallClick(elBall, maxDiameter) {
     elBall.innerText = +elBall.innerText + randomAmount
 }
 
+function onBall2Click(elBall, maxDiameter) {
+    const currHeight = +elBall.innerText
+    const currWidth = +elBall.innerText
+    const randomAmount = getRandomInt(20, 60)
+    if (currHeight >= maxDiameter || (currWidth + randomAmount) >= maxDiameter) {
+        elBall.style.width = 50 + 'px'
+        elBall.style.height = 50 + 'px'
+        elBall.innerText = 50
+        return
+    }
+    elBall.style.width = currWidth + randomAmount + 'px'
+    elBall.style.height = currHeight + randomAmount + 'px'
+    elBall.innerText = +elBall.innerText + randomAmount
+}
+
 function onBall3Click(elBall, ball1, ball2) {
-    console.log('hi:')
+    const elball1 = document.querySelector(ball1)
+    const elball2 = document.querySelector(ball2)
+
+    elball1.classList = ''
+    elball2.classList = ''
+    elball1.classList.add('ball2')
+    elball2.classList.add('ball')
 }
