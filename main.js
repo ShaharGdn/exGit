97e9b5a5e289bbd2b92028ec3519ff9a4ae4c405
ShaharGdn <shahargadon@gmail.com>
2024-02-01 15:52:15 +0200

    Change font styling in red ball

--- main.js
+++ main.js
@@ -3,6 +3,12 @@
 function onBallClick(elBall) {
     const currHeight = +elBall.innerText
     const currWidth = +elBall.innerText
+    if (currHeight >= 400) {
+        elBall.style.width = 100 + 'px'
+        elBall.style.height = 100 + 'px'
+        elBall.innerText = 100
+        return
+    }
     elBall.style.width = currWidth + 50 + 'px'
     elBall.style.height = currHeight + 50 + 'px'
     elBall.innerText = +elBall.innerText + 50
