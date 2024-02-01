acb79815af0045678b10546cefd671f9bfdbc4f2
ShaharGdn <shahargadon@gmail.com>
2024-02-01 17:27:53 +0200

    Add a 4th ball implement event handler

--- main.js
+++ main.js
@@ -39,3 +39,31 @@ function onBall3Click(elBall, ball1, ball2) {
     elball1.classList.add('ball2')
     elball2.classList.add('ball')
 }
+
+function onBall4Click(elBall, ball1, ball2, minDiameter) {
+    const elball1 = document.querySelector(ball1)
+    const elball2 = document.querySelector(ball2)
+
+    const ball1Size = +elball1.innerText
+    const ball2Size = +elball2.innerText
+
+    const randomAmount = getRandomInt(20, 60)
+
+    if (ball1Size <= minDiameter || (ball1Size - randomAmount) <= minDiameter) {
+        elball1.style.width = 100 + 'px'
+        elball1.style.height = 100 + 'px'
+        elball1.innerText = 100
+        elball2.style.width = 100 + 'px'
+        elball2.style.height = 100 + 'px'
+        elball2.innerText = 100
+        return
+    }
+    
+    elball1.style.width = ball1Size - randomAmount + 'px'
+    elball1.style.height = ball1Size - randomAmount + 'px'
+    elball1.innerText = +elball1.innerText - randomAmount
+    
+    elball2.style.width = ball2Size - randomAmount + 'px'
+    elball2.style.height = ball2Size - randomAmount + 'px'
+    elball2.innerText = +elball2.innerText - randomAmount
+}
