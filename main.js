15fd6f0c6af3f89ba922fd684e1dd36becdcf4b1
ShaharGdn <shahargadon@gmail.com>
2024-02-01 16:10:58 +0200

    Increment ball by a random num

--- main.js
+++ main.js
@@ -3,13 +3,14 @@
 function onBallClick(elBall) {
     const currHeight = +elBall.innerText
     const currWidth = +elBall.innerText
-    if (currHeight >= 400) {
+    const randomAmount = getRandomInt(20, 60)
+    if (currHeight >= 400 || (currWidth + randomAmount) >= 400) {
         elBall.style.width = 100 + 'px'
         elBall.style.height = 100 + 'px'
         elBall.innerText = 100
         return
     }
-    elBall.style.width = currWidth + 50 + 'px'
-    elBall.style.height = currHeight + 50 + 'px'
-    elBall.innerText = +elBall.innerText + 50
+    elBall.style.width = currWidth + randomAmount + 'px'
+    elBall.style.height = currHeight + randomAmount + 'px'
+    elBall.innerText = +elBall.innerText + randomAmount
 }
