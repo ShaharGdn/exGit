1356f8d9431fc8be1a01e13e46b4f784d95c7460
ShaharGdn <shahargadon@gmail.com>
2024-02-02 00:48:32 +0200

    Add random color to util

--- util.js
+++ util.js
@@ -1,3 +1,15 @@
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+function getRandomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    
+    var hexR = r.toString(16).padStart(2, '0'); // Convert to hex and ensure two digits
+    var hexG = g.toString(16).padStart(2, '0');
+    var hexB = b.toString(16).padStart(2, '0');
+    
+    return '#' + hexR + hexG + hexB;
+}
